453a4243bf425fa9ee1179acbd979bd798dd57bf
nori229 <50017012+nori229@users.noreply.github.com>
2023-12-11 23:55:58 +0900

    initial commit

---
+++ frontend/app/app/page.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+async function getPost() {
+  const res = await fetch('https://backend:3000/posts');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch Post');
+  }
+
+  return res.json();
+}
+
+const Home: FC = async () => {
+  const posts: Post[] = await getPost();
+
+  return (
+    <div className="m-4">
+      <h1 className="text-4xl mb-4 underline">Post List</h1>
+      {posts.map((post) => (
+        <p className="mb-1" key={post.id}>
+          {post.title}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
